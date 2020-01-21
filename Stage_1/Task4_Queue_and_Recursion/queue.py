adc303cac1a4787a482dd0354fb6023721475492
Pyabecedarian <42270798+Pyabecedarian@users.noreply.github.com>
2020-01-21 20:54:48 +0800

    update

---
+++ Stage_1/Task4_Queue_and_Recursion/queue.py
@@ -0,0 +1,17 @@
+"""
+Queue is structured as an ordered collection of items which are added at one end, called 'rear', and removed
+from the other end, called 'front'. Queues maintain a FIFO ordering property.
+
+Possible operations are:
+    > Queue():
+    > enqueue():
+    > dequeue():
+    > isEmpty():
+"""
+from datastruct import LinkedList
+
+
+
+class Queue(object):
+    def __init__(self):
+        pass
